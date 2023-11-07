1c26c92e98d3b2a231dfea9de2ba689c1ca87d3e
unknown <macchimateo@gmail.com>
2023-11-06 22:44:14 -0300

    Se agrega aspx error al loguearse para avisar al usuario

---
+++ WebApplication1/ErrorLogin.aspx
@@ -0,0 +1,9 @@
+﻿<%@ Page Title="" Language="C#" MasterPageFile="~/master.Master" AutoEventWireup="true" CodeBehind="ErrorLogin.aspx.cs" Inherits="WebApplication1.ErrorLogin"%>
+<asp:Content ID="Content1" ContentPlaceHolderID="head" runat="server">
+</asp:Content>
+<asp:Content ID="Content2" ContentPlaceHolderID="ContentPlaceHolder1" runat="server">
+ 
+    <h1 class="h1">Hubo un Problema</h1>
+
+
+    </asp:Content>
---
+++ WebApplication1/ErrorLogin.aspx.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace WebApplication1
+{
+    public partial class ErrorLogin : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+          /*  if (Session["error"] != null)
+            {
+                lblMensaje.Text = Session["error"].ToString();
+          
+            }
+          */
+        }
+
+
+        protected void btnLogin_Click(object sender, EventArgs e)
+        {
+            Response.Redirect("Login.aspx");
+        }
+
+     
+    }
+}
