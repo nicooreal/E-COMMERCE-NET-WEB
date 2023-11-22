99b6d3450c08b2565285e2531bb000123ed01796
nicooreal <nicoreal99@gmail.com>
2023-11-22 00:01:10 -0300

    pongo estadao en las tablas que faltaban

--- E-COMMERCE.sql
+++ E-COMMERCE.sql
@@ -74,11 +74,6 @@ CREATE TABLE Usuarios (
 )
 
 
-insert into Usuarios(Usuario, Pass, TipoUser)
-values ('nico', 'nico',1)
-
-insert into Usuarios(Usuario, Pass, TipoUser)
-values ('NICO', 'NICO',2)
 
 CREATE TABLE Detalles_Venta ( --Productos Vendidos---
   idVenta INT not null,
@@ -128,6 +123,19 @@ CREATE TABLE Vendedores (
 );
 
 
+/*pongo estado en donde faltaba*/
+alter table Ventas
+add entregado int not null  check(entregado = 1 or entregado = 0) default  0
+
+alter table Compras
+add entregado int not null  check(entregado = 1 or entregado = 0) default  0
+
+
+alter table Clientes
+add estado int not null  check(estado = 1 or estado = 0) default  1
+
+alter table Proveedores
+add estado int not null  check(estado = 1 or estado = 0) default  1
 
 
 
@@ -136,57 +144,19 @@ CREATE TABLE Vendedores (
 
 
 
-SELECT P.estado as Estado, Prov.nombre as NombreProveedor, Prov.idProveedor as idProveedor , P.idProducto AS Id, P.nombre AS Nombre, 
-P.precioCompra AS PrecioCompra, C.idCategoria as idCategoria, C.nombre as NombreCategoria, P.precioVenta AS PrecioVenta, M.nombre as NombreMarca,M.idMarca as idMarca, 
-C.nombre AS NombreCategoria, P.stockActual AS StockActual, P.stockMinimo AS StockMinimo, 
-P.descripcion as Descripcion   FROM Productos P  INNER JOIN Marcas M ON P.idMarca = M.idMarca inner join Categorias C on P.idCategoria = C.idCategoria inner join Proveedores prov on prov.idProveedor = P.idProveedor
-  
-
-update Productos set estado = 1
-
-
-INSERT INTO Productos (nombre, precioCompra, precioVenta, idMarca, idCategoria, stockActual, stockMinimo, estado, descripcion, idProveedor)
-VALUES ('@nombre', @precioCompra,@precioVenta ,@idMarca , @idCategoria, @stockActual,@stockMinimo, 1, '@descripcion', @idProveedor);
-
-
-INSERT INTO Detalles_Venta (idVenta, idProducto, cantidad, precio)
-VALUES (2, 2, 2, 15.50),
-       (2, 1, 1, 30.00);
-
--- Insertar datos de vendedores en la tabla
-INSERT INTO Vendedores (nombre, direccion, telefono, correo)
-VALUES 
-  ('Juan Pérez', 'Calle A #123', '123-456-7890', 'juan@example.com'),
-  ('María Gómez', 'Avenida B #456', '987-654-3210', 'maria@example.com'),
-  ('Carlos Rodríguez', 'Carrera C #789', '555-123-4567', 'carlos@example.com');
 
 
 
-INSERT INTO Ventas (idCliente, fecha, observacion)
-VALUES (1, '2023-11-15', 'Venta de productos al cliente habitual');
 
-select * from Ventas
-select * from Detalles_Venta
-select * from Vendedores
-select * from Clientes
 
-update Ventas set idVendedor = 2 where idCliente = 1
+
 
 
 
 	
-	
-	SELECT V.idVenta, SUM(DetV.cantidad * DetV.precio) as totalVenta, SUM( DetV.cantidad) as cantidadProductos, C.nombreFantasia as nombreCliente,  V.fecha as fecha, V.idCliente as idCliente,  V.observacion as observacion, V.idVendedor as idVendedor, vend.nombre as nombreVendedor   FROM  Ventas V   INNER JOIN Vendedores vend ON vend.idVendedor = V.idVendedor  INNER JOIN Clientes C ON V.idCliente = C.idCliente  INNER JOIN Detalles_Venta DetV ON DetV.idVenta = V.idVenta  GROUP BY  V.idVenta, C.nombreFantasia, V.fecha, V.idCliente, V.observacion, V.idVendedor, vend.nombre;
 
 
-select DetV.cantidad as cantidadPRoductos, DetV.precio, (DetV.precio * DetV.cantidad) as totalPorProducto, P.nombre as nombreProducto,  V.idVenta as idVenta from Ventas V  
-inner join Vendedores vend on vend.idVendedor = V.idVendedor 
-inner join Detalles_Venta DetV on DetV.idVenta = V.idVenta 
-inner join Productos P on P.idProducto = DetV.idProducto
-      
 
 
-	  INSERT INTO Productos (nombre, precioCompra, precioVenta, idMarca, idCategoria, stockActual, stockMinimo, estado, descripcion, idProveedor)
-VALUES ('play 4', 5,6 ,2 , 2, 5,3, 1, 'descripcion qcyo', 2);
 
-select * from Proveedores
+eedores
