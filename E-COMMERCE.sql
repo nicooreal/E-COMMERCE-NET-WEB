33e13f5ce877f82e8ebce09f7cbeceb5b37854ae
nicooreal <nicoreal99@gmail.com>
2023-12-21 19:07:17 -0300

    detalles de la base actualizados

--- E-COMMERCE.sql
+++ E-COMMERCE.sql
@@ -1,70 +1,5 @@
+
 Create Database ECOMMERCE_DB
-use ECOMMERCE_DB
-CREATE TABLE Clientes (
-  idCliente INT PRIMARY KEY IDENTITY (1, 1),
-  nombreFantasia VARCHAR(50) null,
-  direccion VARCHAR(100) null,
-  telefono VARCHAR(20) null,
-  correo VARCHAR(50) null,
-  fechaNacimiento date not null 
-)
-
-select * from compras
-
-CREATE TABLE Proveedores (
-  idProveedor INT PRIMARY KEY IDENTITY (1, 1),
-  nombre VARCHAR(50) not null,
-  direccion VARCHAR(50) null,
-  telefono VARCHAR(20) null,
-  correo VARCHAR(50) null
-)
-
-Go
-CREATE TABLE Marcas (
-  idMarca INT PRIMARY KEY IDENTITY (1, 1) ,
-  nombre VARCHAR(50) not null
-)
-Go
-CREATE TABLE Categorias (
-  idCategoria INT PRIMARY KEY IDENTITY (1, 1),
-  nombre VARCHAR(50) NOT NULL
-)
-
-
-select * from Productos
-
-CREATE TABLE Productos (
-  idProducto INT PRIMARY KEY IDENTITY (1, 1),
-  nombre VARCHAR(50) not null,
-  precioCompra DECIMAL(10, 2) check(precioCompra > 0) not null,
-  precioVenta  DECIMAL(10, 2) not null check(precioVenta > 0),
-  idMarca INT not null,
-  idCategoria INT not null,
-  stockActual INT not null,
-  stockMinimo INT not null,
-  FOREIGN KEY (idMarca) REFERENCES Marcas(idMarca),
-  FOREIGN KEY (idCategoria) REFERENCES Categorias(idCategoria)
-)
-
-
-CREATE TABLE Compras (
-  idCompra INT PRIMARY KEY IDENTITY (1, 1),
-  idProveedor INT not null,
-  fecha DATE not null,
-  observacion varchar (100) NULL,
-  metodoPago char(1) not null check (metodoPago = 'E' or metodoPago = 'T')
-  FOREIGN KEY (idProveedor) REFERENCES Proveedores(idProveedor)
-
-)
-
-
-CREATE TABLE Ventas (
-  idVenta INT PRIMARY KEY IDENTITY (1, 1),
-  idCliente INT not null,
-  fecha DATE not null,
-  observacion varchar(100) null
-  FOREIGN KEY (idCliente) REFERENCES Clientes(idCliente)
-)
 
 
 CREATE TABLE Usuarios (
@@ -75,9 +10,74 @@ CREATE TABLE Usuarios (
     CONSTRAINT [PK_Usuarios] PRIMARY KEY CLUSTERED ([Id] ASC)
 )
 
-INSERT INTO Usuarios( Usuario, PASS, TipoUser) VALUES ('NIK', 'NIK' ,2)
-INSERT INTO Usuarios( Usuario, PASS, TipoUser) VALUES ('MATEO', 'MATEO' ,1)
+go
+CREATE TABLE Marcas (
+  idMarca INT PRIMARY KEY IDENTITY (1, 1) ,
+  nombre VARCHAR(50) not null
+)
+go
+CREATE TABLE Categorias (
+  idCategoria INT PRIMARY KEY IDENTITY (1, 1),
+  nombre VARCHAR(50) NOT NULL
+)
+go
+CREATE TABLE Clientes (
+  idCliente INT PRIMARY KEY IDENTITY (1, 1),
+  nombreFantasia VARCHAR(50) null,
+  direccion VARCHAR(100) null,
+  telefono VARCHAR(20) null,
+  correo VARCHAR(50) null,
+  fechaNacimiento date not null,
+  estado int not null  check(estado = 1 or estado = 0) default  1
+)
 
+CREATE TABLE Vendedores (
+  idVendedor INT PRIMARY KEY IDENTITY (1, 1),
+  nombre VARCHAR(50) not null,
+  direccion VARCHAR(50) null,
+  telefono VARCHAR(20) null,
+  correo VARCHAR(50) null,
+  estado int not null  check(estado = 1 or estado = 0) default  1
+);
+
+go
+CREATE TABLE Ventas (
+  idVenta INT PRIMARY KEY IDENTITY (1, 1),
+  idCliente INT not null,
+  fecha DATE not null,
+  observacion varchar(100) null,
+  idVendedor int null foreign key (idVendedor) references Vendedores,
+  entregado varchar(50) not null   default 'PENDIENTE',
+  FOREIGN KEY (idCliente) REFERENCES Clientes(idCliente)
+)
+go
+
+CREATE TABLE Proveedores (
+  idProveedor INT PRIMARY KEY IDENTITY (1, 1),
+  nombre VARCHAR(50) not null,
+  direccion VARCHAR(50) null,
+  telefono VARCHAR(20) null,
+  correo VARCHAR(50) null,
+  estado int not null  check(estado = 1 or estado = 0) default  1  
+)
+go
+CREATE TABLE Productos (
+  idProducto INT PRIMARY KEY IDENTITY (1, 1),
+  nombre VARCHAR(50) not null,
+  precioCompra DECIMAL(10, 2) check(precioCompra > 0) not null,
+  precioVenta  DECIMAL(10, 2) not null check(precioVenta > 0),
+  idMarca INT not null,
+  idCategoria INT not null,
+  stockActual INT not null,
+  stockMinimo INT not null,
+  estado int not null  check(estado = 1 or estado = 0) default  1,
+  descripcion varchar(50) null,
+  idProveedor int not null foreign key (idProveedor) references Proveedores,
+  FOREIGN KEY (idMarca) REFERENCES Marcas(idMarca),
+  FOREIGN KEY (idCategoria) REFERENCES Categorias(idCategoria)
+)
+
+go
 CREATE TABLE Detalles_Venta ( --Productos Vendidos---
   idVenta INT not null,
   idProducto INT not null,
@@ -87,6 +87,20 @@ CREATE TABLE Detalles_Venta ( --Productos Vendidos---
   FOREIGN KEY (idProducto) REFERENCES Productos(idProducto)
 )
 
+Go
+
+Go
+CREATE TABLE Compras (
+  idCompra INT PRIMARY KEY IDENTITY (1, 1),
+  idProveedor INT not null,
+  fecha DATE not null,
+  observacion varchar (100) NULL,
+  metodoPago char(1) not null check (metodoPago = 'E' or metodoPago = 'T'),
+  entregado int not null  check(entregado = 1 or entregado = 0) default  0,
+  Estado varchar(20) default 'PENDIENTE'
+  FOREIGN KEY (idProveedor) REFERENCES Proveedores(idProveedor)
+
+)
 
 
 CREATE TABLE Detalles_Compra (
@@ -98,104 +112,56 @@ CREATE TABLE Detalles_Compra (
   FOREIGN KEY (idProducto) REFERENCES Productos(idProducto)
 )
 
-/* en productos se agrego descripcion, estado y proveedor
-*/
-alter table Productos
-add estado int not null  check(estado = 1 or estado = 0) default  1,
-descripcion varchar(50) null,
-idProveedor int not null foreign key (idProveedor) references Proveedores
-
-/* en Ventas se agrego id de vendedor
-*/
-alter table Ventas
-add idVendedor int null foreign key (idVendedor) references Vendedores
 
 
-/*se creo la tabla vendedores*/
+create procedure SP_detalleventa  @idVenta int,  @idProducto int,  @cantidadDeProductos int,  @precio int    as  INSERT INTO Detalles_Venta (idVenta, idProducto, cantidad, precio)  VALUES (@idVenta, @idProducto, @cantidadDeProductos, @precio);   
 
+CREATE PROCEDURE SP_restarStock  @cantidadDeProductos int,  @idProducto int  as  update Productos set stockActual = stockActual - @cantidadDeProductos  where idProducto = @idProducto  
 
-CREATE TABLE Vendedores (
-  idVendedor INT PRIMARY KEY IDENTITY (1, 1),
-  nombre VARCHAR(50) not null,
-  direccion VARCHAR(50) null,
-  telefono VARCHAR(20) null,
-  correo VARCHAR(50) null,
-  estado int not null  check(estado = 1 or estado = 0) default  1
-);
+CREATE PROCEDURE SP_sumarStock  @cantidadDeProductos int,  @idProducto int  as  update Productos set stockActual = stockActual + @cantidadDeProductos  where idProducto = @idProducto  
 
-
-/*pongo estado en donde faltaba*/
-alter table Ventas
-add entregado varchar(50) not null   default 'PENDIENTE'
-
-alter table Compras
-add entregado int not null  check(entregado = 1 or entregado = 0) default  0
-
-
-alter table Clientes
-add estado int not null  check(estado = 1 or estado = 0) default  1
-
-alter table Proveedores
-add estado int not null  check(estado = 1 or estado = 0) default  1
-
-
-
-
-update Ventas set entregado = 0 where idVenta = @id
-
-
-select idVenta from Ventas
-Select Id, usuario, pass, tipouser from Usuarios
-DELETE FROM USUARIOS
-
-
-	
-SELECT V.idVenta, V.entregado as entregado, SUM(DetV.cantidad * DetV.precio) as totalVenta, SUM( DetV.cantidad) as cantidadProductos, C.nombreFantasia as nombreCliente,
-V.fecha as fecha, V.idCliente as idCliente,  V.observacion as observacion, V.idVendedor as idVendedor, vend.nombre as nombreVendedor   
-FROM  Ventas V   INNER JOIN Vendedores vend ON vend.idVendedor = V.idVendedor  
-INNER JOIN Clientes C ON V.idCliente = C.idCliente  
-INNER JOIN Detalles_Venta DetV ON DetV.idVenta = V.idVenta  GROUP BY  V.idVenta, C.nombreFantasia, V.fecha, V.idCliente, V.observacion, V.idVendedor, vend.nombre, V.entregado
-
-
-
-insert into Marcas(nombre) values ('BONAFIDE')
-
-
-select * from Compras
-select * from Detalles_Compra
-select * from Productos
-delete from Compras
-DBCC CHECKIDENT ('Compras', RESEED,0)  
-
-
-
-select * from ventas
-
-
-
-INSERT INTO Compras (idProveedor, fecha, observacion, estado) VALUES (1,GETDATE(), 'buena compra', 'pendiente')
-
-
-
-create procedure SP_detalleCompra
-@idCompra int,
-@idProducto int,
-@cantidadDeProductos int,
-@precio int
-
-as
-INSERT INTO Detalles_Compra (idCompra, idProducto, cantidad, precio)
-VALUES (@idCompra, @idProducto, @cantidadDeProductos, @precio); 
-
-select C.idCompra as idCompra, C.fecha as fecha, C.idProveedor as proveedor,C.entregado as entregado ,C.observacion as observacion,SUM(DetC.cantidad * DetC.precio) as totalCompra ,SUM( DetC.cantidad) as cantidadProductos from Compras C inner join Detalles_Compra DetC on C.idCompra = DetC.idCompra
-
-
-
-SELECT C.idCompra AS idCompra, C.fecha AS fecha, C.idProveedor AS proveedor ,C.estado AS estado, C.observacion AS observacion, SUM(DetC.cantidad * DetC.precio) AS totalCompra, SUM(DetC.cantidad) AS cantidadProductos,P.nombre FROM Compras C INNER JOIN Detalles_Compra DetC ON C.idCompra = DetC.idCompra inner join Proveedores P on P.idProveedor = C.idProveedor GROUP BY C.idCompra, C.fecha, C.idProveedor,  C.estado, C.observacion, P.nombre;
+create procedure SP_detalleCompra  @idCompra int,  @idProducto int,  @cantidadDeProductos int,  @precio int    as  INSERT INTO Detalles_Compra (idCompra, idProducto, cantidad, precio)  VALUES (@idCompra, @idProducto, @cantidadDeProductos, @precio);     
 
 
 
 
 
 
-select DetC.cantidad as cantidadPRoductos, P.idProducto as idProducto ,DetC.precio, (DetC.precio * DetC.cantidad) as totalPorProducto, P.nombre as nombreProducto,  C.idCompra as idCompra from Compras C   inner join Detalles_Compra DetC on DetC.idCompra = C.idCompra inner join Productos P on P.idProducto = DetC.idProducto
+
+--EJEMPLOS PARA BASE
+
+
+INSERT INTO Usuarios (Usuario, Pass, TipoUser)
+VALUES ('NIK', '1234', 2),
+       ('MATEO', '1234', 1);
+
+
+
+	   INSERT INTO Marcas (nombre)
+VALUES ('Marca A'),
+       ('Marca B'),
+       ('Marca C');
+
+
+INSERT INTO Categorias (nombre)
+VALUES ('Categoría 1'),
+       ('Categoría 2'),
+       ('Categoría 3');
+
+
+INSERT INTO Proveedores (nombre, direccion, telefono, correo)
+VALUES ('Proveedor 1', 'Dirección 1', '123456789', 'proveedor1@ejemplo.com'),
+       ('Proveedor 2', 'Dirección 2', '987654321', 'proveedor2@ejemplo.com');
+
+
+
+INSERT INTO Vendedores (nombre, direccion, telefono, correo)
+VALUES ('Vendedor 1', 'Dirección 1', '111111111', 'vendedor1@ejemplo.com'),
+       ('Vendedor 2', 'Dirección 2', '222222222', 'vendedor2@ejemplo.com');
+
+
+INSERT INTO Clientes (nombreFantasia, direccion, telefono, correo, fechaNacimiento)
+VALUES ('Cliente A', 'Dirección A', '555555555', 'clienteA@ejemplo.com', '2000-01-01'),
+       ('Cliente B', 'Dirección B', '666666666', 'clienteB@ejemplo.com', '1995-05-05');
+
+
